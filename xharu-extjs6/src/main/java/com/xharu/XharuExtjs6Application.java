6a8e3b1cbb7fc5dc68e625a04ae99d60ed6407e7
oopchoi <oopchoi@gmail.com>
2016-02-22 20:39:35 +0900

    Ext JS 6 애플리케이션을 위한 JPA, MySQL, 스프링 부트 게시판 템플릿 소스

--- xharu-extjs6/src/main/java/com/xharu/XharuExtjs6Application.java
+++ xharu-extjs6/src/main/java/com/xharu/XharuExtjs6Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class XharuExtjs6Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(XharuExtjs6Application.class, args);
 	}
