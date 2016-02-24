5c8478bb2c77de852c16125601237decff7cbbfd
oopchoi <oopchoi@gmail.com>
2016-02-24 23:46:41 +0900

    Ext JS 6 애플리케이션을 위한 JPA, MySQL, 스프링 부트 게시판 템플릿 소스

---
+++ xharu-extjs6/src/main/resources/static/app/build/temp/development/App/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/Users/oopchoi/development/Git/workspace_intellij/xharu/xharu-extjs6/src/main/resources/static/app/build/.sass-cache'
